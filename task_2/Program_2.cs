4426eb66f3e249f3f8f0c7e9b50122c4f2d337a1
Pavel Kulik <Kanstantin0105@gmail.com>
2023-04-25 23:23:50 +0300

    add task_2

---
+++ task_2/Program_2.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool checkNumber(int date)
+        {
+            if (date > 99 && date < 1000) return true;
+            else return false;
+        }       
+
+        int findThirdNumber(int date)
+        {
+            while (!checkNumber(date))
+            {
+                date = date / 10;
+            }
+            return date % 10;
+        }
+
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine()!);
+        
+        Console.Write(number + " -> ");
+        if (number < 100) Console.WriteLine("третьей цифры нет");
+        else
+        {
+            int result = findThirdNumber(number);
+            Console.WriteLine(result);
+        }
+        
+    }
+}
