7b91f523911edb657d084a43e6806fee082bd8b4
Pavel Kulik <Kanstantin0105@gmail.com>
2023-04-25 22:58:28 +0300

    add .gitignore

---
+++ task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        bool checkNumber(int date)
+        {
+            if (date > 99 && date < 1000) return true;
+            else return false;
+        }
+
+        int secondDate(int date) => date / 10 % 10;
+
+        Console.Write("Введите трёхзначное число: ");
+        int number = int.Parse(Console.ReadLine()!);
+
+        if (!checkNumber(number)) Console.WriteLine("Введино не трёхзначное число!");
+        else
+        {
+            int result = secondDate(number);
+            Console.WriteLine(number + " -> " + result);
+        }
+    }
+}
